87dcdafd4a6e315803109d508bae9b69d7130ab6
A-serena <52956355+A-serena@users.noreply.github.com>
2020-02-28 19:45:43 +0900

    コードとりどみの登録

---
+++ py_square_root.py
@@ -0,0 +1,36 @@
+print("hallo world!")
+#xの平方根を求める
+
+while True:
+    x = input("正の数値を入力してください ")
+    try:
+        x = float(x)        
+    except ValueError:
+        print(x,"は数値に変換できません")
+        continue #continue は 次へ移る の意味 やり直し ではない！
+    except:
+        print("予期していないエラーです")
+        exit()
+    if(x <=0):
+        print(x,"は正の値ではありません")
+        continue
+    
+    print(str(x),"の平方根を今から求めます")
+    #x =float(x) #inputで得た値は文字列(str)なので数扱いにさせる
+    
+    rnew = x
+    
+    while True:
+        diff = rnew - x/rnew
+        diff = abs(diff)
+        while(diff > 1.0E-6): #差が10^-6より大きければ繰り返す
+            r1 = rnew
+            r2 = x/r1
+            rnew = (r1 + r2)/2
+            diff = r1 - r2
+            diff = abs(diff)
+        
+        print(str(rnew))
+        break
+            
+
